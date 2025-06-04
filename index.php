b4a37731b7f1851cfb2ed21f5b42560fb3d791ba
Julia Abreu <julia14.amarques@gmail.com>
2025-06-04 17:41:29 -0300

    teste
    só quero testar o php e como usa do xampp e localhost

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste</title>
+</head>
+<body> 
+    <p>é isso ai</p>
+
+    <?php
+        echo("foda");
+        $nome = "Julia";
+        echo($nome."linda");
+    ?>
+
+</body>
+</html>
